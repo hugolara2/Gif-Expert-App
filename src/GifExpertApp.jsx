1ec2a5109e562da8e3a29712071e99795b0cf589
Hugo Lara <hugo.alex200197@outlook.com>
2024-08-21 10:47:42 -0600

    Adding validation, checks if categories exists in the array

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
+import { AddCategory } from "./Components/AddCategory";
 
 export const GifExpertApp = () => {
+   
+   const [categories, setCategories] = useState(['Naruto', 'Metal Gear']);
+   
+   const onAddCategory = (newCategory) => {
+      
+      if(categories.find(category => category.toLowerCase() === newCategory.toLowerCase())) { 
+         return;
+      }
+
+      setCategories([newCategory, ...categories]);
+   }
+
    return(
       <>
          <h1>Gif Expert App</h1>
+         <AddCategory 
+            //addCategory={ setCategories }
+            onNewCategory={ onAddCategory }
+         />
+         <ol>
+            {categories.map(category => {
+               return <li key={category}>{category}</li>
+            })}
+         </ol>
       </>
    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode }from 'react';
 import { createRoot } from 'react-dom/client';
-import { GifExpertApp } from './GifExpertApp'
-
+import { GifExpertApp } from './GifExpertApp';
+import './Styles/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
